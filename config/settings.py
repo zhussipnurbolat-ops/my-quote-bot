18da4a735f34c174a496de008d0281c270730ffe
zhussipnurbolat-ops <zhussipnurbolat@gmail.com>
2026-05-19 16:21:39 +0500

    Security: Move SECRET_KEY to environment variables

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'MY_SECRET_KEY'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-local-development-dummy-key-12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
